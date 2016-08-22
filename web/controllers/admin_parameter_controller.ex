239f747dc53f74a7862a053e8f07e67da53a7b91
Andy Nagels <thereisanewway@gmail.com>
2016-08-22 11:11:11 +0200

    Added templates + controller for the admin_parameter screens.

--- web/controllers/admin_parameter_controller.ex
+++ web/controllers/admin_parameter_controller.ex
@@ -1,9 +1,25 @@
 defmodule Area91.AdminParameterController do
   use Phoenix.Controller
 
+  alias Area91.Router
+  import Area91.Router.Helpers
+
   plug :action
 
   def parameter(conn, _params) do
-    render conn, "parameter.html"
+    conn
+    |> assign(:parameters, Area91.Repo.all(Area91.Parameter))
+    |> render("parameter.html")
   end
+
+  def new(conn, _params) do
+    render(conn, "new.html")
+  end
+
+  def create(conn, %{"parameter" => %{"name" => name, "description" => description}}) do
+    l_parameter = %Area91.Parameter{name: name, description: description}
+    Area91.Repo.insert(l_parameter)
+    redirect(conn, to: admin_parameter_path(conn, :overview))
+  end
+
 end
