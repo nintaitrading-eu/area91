0f748ba6258fcad74dcc41c0fdc97fbadfb9b675
Andy Nagels <thereisanewway@gmail.com>
2016-08-22 16:52:24 +0200

    Generalisation: commodity -> product

--- priv/repo/migrations/20160822143856_create_table__t__product_type.exs
+++ priv/repo/migrations/20160822143856_create_table__t__product_type.exs
@@ -1,21 +1,21 @@
-defmodule Area91.Repo.Migrations.CreateTableTCommodityType do
+defmodule Area91.Repo.Migrations.CreateTableTProductType do
   use Ecto.Migration
 
   def up do
-    execute "CREATE TABLE T_COMMODITY_TYPE
+    execute "CREATE TABLE T_PRODUCT_TYPE
     (
-        commodity_type_id serial not null,
+        product_type_id serial not null,
         name varchar(50) not null,
         description varchar(256) not null default '',
         is_active int not null default 1,
         date_created timestamp not null default '1900-01-01',
         date_modified timestamp not null default '1900-01-01',
-        constraint pk_commodity_type_id primary key(commodity_type_id)
+        constraint pk_product_type_id primary key(product_type_id)
     );"
   end
 
   def down do
-    execute "drop table T_COMMODITY_TYPE;"
+    execute "drop table T_PRODUCT_TYPE;"
   end
 
   def change do
