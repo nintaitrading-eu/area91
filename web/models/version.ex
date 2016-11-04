123387ac1c9bbf72a51c42e45481a89baa79b6b0
Andy Nagels <thereisanewway@gmail.com>
2016-11-04 11:10:24 +0100

    Get version info from the database.

---
+++ web/models/version.ex
@@ -0,0 +1,27 @@
+defmodule Area91.Version do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  schema "t_version" do
+    field :database_version, :string
+    field :database_version_info, :string
+    field :application_version, :string
+    field :application_version_info, :string
+    timestamps([{:date_created,:date_modified}])
+  end
+
+  @required_fields ~w(database_version, database_version_info, application_version, application_version_info)
+  @optional_fields ~w()
+
+  @doc """
+  Creates a changeset based on the `model` and `params`.
+
+  If no params are provided, an invalid changeset is returned
+  with no validation performed.
+  """
+  def changeset(model, params \\ :empty) do
+    model
+    |> cast(params, @required_fields, @optional_fields)
+  end
+
+end
