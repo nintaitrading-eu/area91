a69bc51f3efb3cbade0f61f7b263cc03878a4bee
rockwolf <thereisanewway@gmail.com>
2016-11-12 17:22:16 +0100

    Added database + application version.

--- priv/repo/migrations/20161112161227_insert_table__t__version.exs
+++ priv/repo/migrations/20161112161227_insert_table__t__version.exs
@@ -1,6 +1,14 @@
 defmodule Area91.Repo.Migrations.InsertTableTVersion do
   use Ecto.Migration
 
+  def up do
+    execute "insert into t_version values(1, '0.0.1', 'First try!', '0.0.1', 'First try!', current_date, current_date);"
+  end
+
+  def down do
+    execute "delete from t_version;"
+  end
+
   def change do
 
   end
