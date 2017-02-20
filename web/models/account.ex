97391c4c64fa155a871735c050c1f82abc446c22
rockwolf <thereisanewway@gmail.com>
2017-02-21 00:50:05 +0100

    Aaaarg, why does this not work??? I can update changesets via the repl, so it's going to be something small that needs to change. But what?
    The error is: unknown field `name`. Only fields, embeds and associations (except :through ones) are supported in changesets

--- web/models/account.ex
+++ web/models/account.ex
@@ -2,6 +2,7 @@ defmodule Area91.Account do
   use Area91.Web, :model
   #use Ecto.Schema, Ecto.Type
   #import Ecto.Changeset
+  import Ecto.Query
 
   @primary_key {:account_id, :integer, []}
   @derive {Phoenix.Param, key: :account_id}
@@ -12,9 +13,10 @@ defmodule Area91.Account do
     field :is_active, :integer, default: 1
     field :date_created, Ecto.DateTime
     field :date_modified, Ecto.DateTime
+    #belongs_to :account, Area91.Account
   end
 
-  @required_fields ~w(name description)
+  #@required_fields ~w(name description)
   @doc """
   Creates a changeset based on the `model` and `params`.
 
@@ -22,10 +24,10 @@ defmodule Area91.Account do
   with no validation performed.
   """
   # Note: add required params below.
-  def changeset(a_model, a_params \\ %{}) do
-    a_model
-    |> cast(a_params, ~w(name description is_active date_created date_modified))
-    |> validate_required(~w(name description))
+  def changeset(model, params \\ %{}) do
+    model
+    |> cast(params, [:name, :description])
+    |> validate_required([:name, :description])
     #|> unique_constraint([:name])
   end
 
