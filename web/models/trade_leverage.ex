32efcb369d0e8f87f15d27cd4e5677309b9c71e8
Andy Nagels <thereisanewway@gmail.com>
2017-03-17 10:33:09 +0100

    Added the trade_leverage model.

---
+++ web/models/trade_leverage.ex
@@ -0,0 +1,21 @@
+defmodule Area91.TradeLeverage do
+  use Area91.Web, :model
+  import Tuple
+
+  schema "trade_leverage" do
+    field :contracts, :any, virtual: true
+  end
+
+  @doc """
+  Creates a changeset based on the `model` and `params`.
+
+  If no params are provided, an invalid changeset is returned
+  with no validation performed.
+  """
+  def changeset(model, params \\ %{}) do
+    model
+    |> cast(params, [:contracts])
+    |> validate_required([:contracts])
+  end
+
+end
