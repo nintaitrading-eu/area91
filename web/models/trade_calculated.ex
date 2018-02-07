01c29b65ceb331e0d500d0f8b816ea89a5b092da
rockwolf <thereisanewway@gmail.com>
2018-02-07 11:11:19 +0100

    area91#25 Added trade_calculated model code.

---
+++ web/models/trade_calculated.ex
@@ -0,0 +1,44 @@
+defmodule Area91.TradeCalculated do
+  use Area91.Web, :model
+  import Ecto.Query
+
+  @primary_key {:trade_calculated_id, :integer, []}
+  @derive {Phoenix.Param, key: :trade_calculated_id}
+  schema "t_trade_calculated" do
+    field :price_buy, :decimal
+    field :price_sell, :decimal
+    field :amount_buy, :decimal
+    field :amount_sell, :decimal
+    field :amount_buy_total, :decimal
+    field :amount_sell_total, :decimal
+    field :risk_input, :decimal
+    field :risk_initial, :decimal
+    field :risk_initial_percent, :decimal
+    field :cost_total, :decimal
+    field :cost_other, :decimal
+    field :stoploss, :decimal
+    field :stoploss_orig, :decimal
+    field :profit_loss, :decimal
+    field :profit_loss_orig, :decimal
+    field :profit_loss_total, :decimal
+    field :profit_loss_total_percent, :decimal
+    field :r_multiple, :decimal
+    field :is_deleted, :boolean
+    field :date_created, Timex.Ecto.DateTime
+    field :date_modified, Timex.Ecto.DateTime
+  end
+
+  @doc """
+  Creates a changeset based on the `a_model` and `a_params`.
+
+  If no a_params are provided, an invalid changeset is returned
+  with no validation performed.
+  """
+  # Note: add required params below.
+  def changeset(a_model, a_params \\ %{}) do
+    a_model
+    |> cast(a_params, [:price_buy, :price_sell, :amount_buy, :amount_sell, :amount_buy_total, :amount_sell_total, :risk_input, :risk_initial, :risk_initial_percent, :cost_total, :cost_other, :stoploss, :stoploss_orig, :profit_loss, :profit_loss_orig, :profit_loss_total, :profit_loss_total_percent, :r_multiple])
+    |> validate_required([:price_buy, :price_sell, :amount_buy, :amount_sell, :amount_buy_total, :amount_sell_total, :risk_input, :risk_initial, :risk_initial_percent, :cost_total, :cost_other, :stoploss, :stoploss_orig, :profit_loss, :profit_loss_orig, :profit_loss_total, :profit_loss_total_percent, :r_multiple])
+  end
+
+end
