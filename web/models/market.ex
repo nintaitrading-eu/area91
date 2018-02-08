9a45d6a861cc8cc6ba2563c8b4b0d3a6a9276b42
rockwolf <thereisanewway@gmail.com>
2018-02-08 10:25:31 +0100

    area91#25 Cleanup: using @required and @fields.

--- web/models/market.ex
+++ web/models/market.ex
@@ -8,22 +8,24 @@ defmodule Area91.Market do
     field :code, :string
     field :name, :string
     field :country, :string
-    field :is_active, :integer, default: 1
-    timestamps([{:date_created,:date_modified}])
+    field :is_deleted, :boolean
+    field :date_created, :naive_datetime
+    field :date_modified, :naive_datetime
   end
 
-  @required_fields ~w(code name country)
-  @optional_fields ~w()
+  @fields ~w(code name country is_deleted date_created date_modified)
+  @required ~w(code name country is_deleted date_created date_modified)
 
   @doc """
-  Creates a changeset based on the `model` and `params`.
+  Creates a changeset based on the `a_model` and `a_params`.
 
   If no params are provided, an invalid changeset is returned
   with no validation performed.
   """
-  def changeset(model, params \\ :empty) do
-    model
-    |> cast(params, @required_fields, @optional_fields)
+  def changeset(a_model, a_params \\ %{}) do
+    a_model
+    |> cast(a_params, @fields)
+    |> validate_required(@required)
     |> validate_length(:code, max: 50)
     |> validate_length(:name, max: 30)
     |> validate_length(:country, max: 3)
