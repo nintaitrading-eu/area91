ea8f3c88a359734650c3332603c23a81ed66e23a
rockwolf <thereisanewway@gmail.com>
2018-02-08 11:08:39 +0100

    area91#25 Use @fields, @required and :naive_datetime.

--- web/models/product_type.ex
+++ web/models/product_type.ex
@@ -8,9 +8,12 @@ defmodule Area91.ProductType do
     field :name, :string
     field :description, :string
     field :is_deleted, :boolean, default: :false 
-    field :date_created, Timex.Ecto.DateTime
-    field :date_modified, Timex.Ecto.DateTime
+    field :date_created, :naive_datetime
+    field :date_modified, :naive_datetime
   end
+
+  @fields ~w(name description is_deleted date_created date_modified)
+  @required ~w(name description is_deleted date_created date_modified)
 
   @doc """
   Creates a changeset based on the `a_model` and `a_params`.
@@ -20,8 +23,8 @@ defmodule Area91.ProductType do
   """
   def changeset(a_model, a_params \\ %{}) do
     a_model
-    |> cast(a_params, [:name, :description])
-    |> validate_required([:name, :description])
+    |> cast(a_params, @fields)
+    |> validate_required(@required)
   end
 
 end
