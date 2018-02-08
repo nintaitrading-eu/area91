144971a09dcf3ff25e056c1b26ed4457deffb13c
rockwolf <thereisanewway@gmail.com>
2018-02-08 12:01:28 +0100

    area91#25: Use @fields, @required and :naive_datetime.
    Note: This breaks compilation, because we don't have trade_cost etc.
    yet.

--- web/models/trade_journal.ex
+++ web/models/trade_journal.ex
@@ -1,6 +1,5 @@
 defmodule Area91.TradeJournal do
   use Area91.Web, :model
-  import Tuple
 
   schema "trade_journal" do
     field :account_name, :string, virtual: true
@@ -21,9 +20,12 @@ defmodule Area91.TradeJournal do
     field :month_sell, :integer, virtual: true
     field :day_sell, :integer, virtual: true
     field :is_deleted, :boolean, virtual: true
-    field :date_created, Timex.Ecto.DateTime, virtual: true
-    field :date_modified, Timex.Ecto.DateTime, virtual: true
+    field :date_created, :naive_datetime, virtual: true
+    field :date_modified, :naive_datetime, virtual: true
   end
+
+  @fields ~w(account_name trade_calculated product_name product_type_name currency_code pool trade_cost trade_drawdown date_buy date_buy year_buy month_buy day_buy date_sell year_sell month_sell day_sell is_deleted date_created date_modified)
+  @required ~w(account_name trade_calculated product_name product_type_name currency_code pool trade_cost trade_drawdown date_buy date_buy year_buy month_buy day_buy date_sell year_sell month_sell day_sell is_deleted date_created date_modified)
 
   @doc """
   Creates a changeset based on the `a_model` and `a_params`.
@@ -34,34 +36,8 @@ defmodule Area91.TradeJournal do
   # Note: add required params below.
   def changeset(a_model, a_params \\ %{}) do
     a_model
-    |> cast(a_params, [
-        :account_name,
-        :product_name,
-        :product_type_name,
-        :currency_code,
-        :pool,
-        :date_buy,
-        :year_buy,
-        :month_buy,
-        :day_buy,
-        :date_sell,
-        :year_sell,
-        :month_sell,
-        :day_sell])
-    |> validate_required([
-        :account_name,
-        :product_name,
-        :product_type_name,
-        :currency_code,
-        :pool,
-        :date_buy,
-        :year_buy,
-        :month_buy,
-        :day_buy,
-        :date_sell,
-        :year_sell,
-        :month_sell,
-        :day_sell])
+    |> cast(a_params, @fields) 
+    |> validate_required(@required)
   end
 
 end
