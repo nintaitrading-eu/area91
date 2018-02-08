4ad3aa0c6db035b3f4971d7203aeeef443388677
rockwolf <thereisanewway@gmail.com>
2018-02-08 09:55:21 +0100

    area91#25 Use @fields and @required.

--- web/models/import_accounts.ex
+++ web/models/import_accounts.ex
@@ -6,16 +6,19 @@ defmodule Area91.ImportAccounts do
     field :file, :any, virtual: true
   end
 
+  @fields ~w(file)
+  @required ~w(file)
+
   @doc """
-  Creates a changeset based on the `model` and `params`.
+  Creates a changeset based on the `a_model` and `a_params`.
 
   If no params are provided, an invalid changeset is returned
   with no validation performed.
   """
-  def changeset(model, params \\ %{}) do
-    model
-    |> cast(params, [:file])
-    |> validate_required([:file])
+  def changeset(a_model, a_params \\ %{}) do
+    a_model
+    |> cast(a_params, @fields)
+    |> validate_required(@required)
   end
 
 end
