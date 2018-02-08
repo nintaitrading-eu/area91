cda5ae346ab02855855b2ac1caa1530953097f7b
rockwolf <thereisanewway@gmail.com>
2018-02-08 14:24:00 +0100

    area91#25 Added migration script for T_TRADE_DRAWDOWN_HIST.

---
+++ priv/repo/migrations/20160219123752_create_table__t__trade_drawdown_hist.exs
@@ -0,0 +1,28 @@
+defmodule Area91.Repo.Migrations.CreateTable_T_TRADE_DRAWDOWN_HIST do
+  use Ecto.Migration
+
+  def up do
+    execute "CREATE TABLE T_TRADE_DRAWDOWN_HIST
+    (
+        trade_drawdown_hist_id bigserial not null,
+        trade_drawdown_id bigint not null,
+        drawdown_current int not null,
+        drawdown_max int not null,
+        is_deleted boolean not null,
+        date_created timestamp not null,
+        date_modified timestamp not null,
+        date_hist_created timestamp not null,
+        date_hist_modified timestamp not null default current_date,
+        constraint pk_trade_drawdown_hist_id primary key(trade_drawdown_id),
+        constraint fk_T_TRADE_DRAWDOWN_HIST_trade_drawdown_id foreign key (trade_drawdown_id) references T_TRADE_DRAWDOWN (trade_drawdown_id)
+    );"
+    execute "COMMENT ON TABLE T_TRADE_DRAWDOWN_HIST IS 'History table for T_TRADE_DRAWDOWN.';"
+  end
+
+  def down do
+    execute "drop table T_TRADE_DRAWDOWN_HIST;"
+  end
+ 
+  def change do
+  end
+end
