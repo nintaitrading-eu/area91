7401fb69cfb6f5b44bd58915db904b6a34363b06
rockwolf <thereisanewway@gmail.com>
2018-02-13 10:51:09 +0100

    area91#25 Fix in t_trade migration. Also set the database to UTC format, to make sure the inserts work correctly.

--- priv/repo/migrations/20170712090201_create_table__t__trade.exs
+++ priv/repo/migrations/20170712090201_create_table__t__trade.exs
@@ -9,7 +9,7 @@ defmodule Area91.Repo.Migrations.CreateTable_T_TRADE do
         product_id bigserial not null,
         pool_id bigserial not null,
         trade_cost_id bigserial not null,
-        drawdown_id bigserial not null,
+        trade_drawdown_id bigserial not null,
         date_buy timestamp with time zone,
         year_buy int,
         month_buy int,
@@ -41,7 +41,7 @@ defmodule Area91.Repo.Migrations.CreateTable_T_TRADE do
     execute "COMMENT ON COLUMN T_TRADE.product_id IS 'Link to the product we are trading.';"
     execute "COMMENT ON COLUMN T_TRADE.pool_id IS 'Link to the available pool.';"
     execute "COMMENT ON COLUMN T_TRADE.trade_cost_id IS 'Link to the table with trade costs, regarding this trade record.';"
-    execute "COMMENT ON COLUMN T_TRADE.drawdown_id IS 'Link to the table with the drawdown, linked to this trade record.';"
+    execute "COMMENT ON COLUMN T_TRADE.trade_drawdown_id IS 'Link to the table with the drawdown, linked to this trade record.';"
     execute "COMMENT ON COLUMN T_TRADE.date_buy IS 'Date on which the product is bought.';"
     execute "COMMENT ON COLUMN T_TRADE.year_buy IS 'Year of date_buy, provided for easy filtering.';"
     execute "COMMENT ON COLUMN T_TRADE.month_buy IS 'Month of date_buy, provided for easy filtering.';"
